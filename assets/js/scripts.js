d3b05c9c7bc09effe4233b0201fb9baa2b2429ea
Thomas Mercer <tmercer74@gmail.com>
2017-10-28 20:06:49 -0700

    fixed html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,22 @@
-$(document).ready(function(){
-      $(".button-collapse").sideNav();
-    }); 
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+});
 
-$(document).ready(function(){
-      $('.parallax').parallax();
-   });﻿ 
+$(document).ready(function() {
+    $('.parallax').parallax();
+});﻿
+
+$('.dropdown-button').dropdown('open');
+$('.dropdown-button').dropdown('close');
+
+$('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+  }
+);
